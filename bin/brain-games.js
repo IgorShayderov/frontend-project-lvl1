19975b2c45fd25bf05e21f220575ed2c508f555c
Igor Shayderov <flashmob123@gmail.com>
2022-03-08 19:49:49 +0200

    Brain-even game
    User should guess is number even or not.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-
 import readlineSync from 'readline-sync';
+import BrainEven from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('What is your name?');
 
 console.log(`Hello, ${userName}!`);
+
+BrainEven(userName).start();
